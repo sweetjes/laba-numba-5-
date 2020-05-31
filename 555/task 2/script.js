fc9781bd2b33da7d73e6b0a8eba949cd69668697
sweetjes <37630210+sweetjes@users.noreply.github.com>
2020-05-31 17:37:02 +0300

    big fixes(no)

--- 555/task 2/script.js
+++ 555/task 2/script.js
@@ -1,12 +1,11 @@
 function isArrayEqual(firstArray, secondArray) {
- if (firstArray.length === secondArray.length && Array.isArray(firstArray) && Array.isArray(secondArray) ) {
-  for (var i=0;firstArray.length > i;i++) {
-    if (firstArray[i] === secondArray[i]) {
-      return true;
- }
-}
-
-} else {
-  return false;
-}
+  if (firstArray.length === secondArray.length && Array.isArray(firstArray) && Array.isArray(secondArray)) {
+    for (var i = 0; firstArray.length > i; i++) {
+      if (firstArray[i] === secondArray[i]) {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
 }
